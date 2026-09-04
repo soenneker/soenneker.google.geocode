3e346c314890d51f903376eb11182030359def02
soenneker <jake@soenneker.com>
2026-09-03 22:01:22 -0500

    Add interface inheritdoc references

--- src/Soenneker.Google.Geocode/GoogleGeocodeUtil.cs
+++ src/Soenneker.Google.Geocode/GoogleGeocodeUtil.cs
@@ -14,6 +14,7 @@ using Soenneker.Extensions.Task;
 
 namespace Soenneker.Google.Geocode;
 
+/// <inheritdoc cref="IGoogleGeocodeUtil" />
 public sealed class GoogleGeocodeUtil : IGoogleGeocodeUtil
 {
     private readonly string _apiKey;
